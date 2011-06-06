c29a55ce72507d3f8355921b09ec1113fb546364
Thiru <thiruns@gmail.com>
2011-06-06 22:12:05 +0200

    added chapter 5 exercises

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,10 @@ module ApplicationHelper
       "#{base_title} | #{@title}"
     end
   end
+
+  #Return log image path.
+  def logo
+    image_tag("logo.png", :alt => "Sample App", :class => "round") 
+  end
+ 
 end
